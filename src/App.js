c02e2e0a7eeb8692cd629e843924a0168a639ca2
DevAleksandarS <spasojevic.dev@gmail.com>
2022-09-28 17:13:19 +0200

    Styling commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import "./css/style.css";
+
 function App() {
   return (
     <div className="main-container">
-      <p>Rock Paper Scissors</p>
+      <div className="small-container">
+        <h1 className="heading">Rock Paper Scissors</h1>
+
+        <div className="game-field">
+          <div>
+            <p>Computer</p>
+            <p>ROCK</p>
+          </div>
+          <div>
+            <p>Player</p>
+            <p>SCISSORS</p>
+          </div>
+        </div>
+
+        <p className="results">0 - 0</p>
+
+        <div className="button-field">
+          <button>ROCK</button>
+          <button>PAPER</button>
+          <button>SCISSORS</button>
+        </div>
+      </div>
     </div>
   );
 }
